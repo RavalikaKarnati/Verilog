c196d8047e998e128b4e6aa2a2b025deacff2494
RavalikaKarnati <ravalika.karnati@gmail.com>
2025-12-07 18:33:12 -0500

    Create CarryLookAheadAdder.v

---
+++ Combinational/CarryLookAheadAdder.v
@@ -0,0 +1,3 @@
+The drawback of ‘Ripple carry adder‘ is that it has a carry propagation delay that introduces slow computation.
+Since adders are used in designs like multipliers and divisions, it causes slowness in their computation. 
+To tackle this issue, a carry look-ahead adder (CLA) can be used that reduces propagation delay with additional hardware complexity
