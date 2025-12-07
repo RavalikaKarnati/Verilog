d2e96667f1e5f60ad1981384164fabc333d23b15
RavalikaKarnati <ravalika.karnati@gmail.com>
2025-12-07 18:24:55 -0500

    Add full subtractor module in Verilog

---
+++ Combinational/full_subtractor.v
@@ -0,0 +1,11 @@
+module full_subtractor(
+    input a, b, Bin,
+    output D, Bout
+    );
+    
+  assign D = a ^ b ^ Bin;
+  assign Bout = (~a & b) | (~(a ^ b) & Bin);
+  
+  // using half subtractor
+  
+endmodule
