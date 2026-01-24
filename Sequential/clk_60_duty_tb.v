0cc60c26522fe6ca249042baff9438bc0f99aeac
RavalikaKarnati <ravalika.karnati@gmail.com>
2026-01-24 14:46:34 -0500

    Add clock generator module for 60% duty cycle

---
+++ Sequential/clk_60_duty_tb.v
@@ -0,0 +1,20 @@
+`define CLK_PERIOD 10ns
+module clk_gen; 
+  realtime on_t = `CLK_PERIOD * 0.6; 
+  realtime off_t = `CLK_PERIOD * 0.4; 
+  bit clk; 
+  
+  always begin 
+    #on_t clk = 0; 
+    #off_t clk = 1; 
+  end 
+  initial begin 
+    clk = 1; 
+    #50 
+    $finish; 
+  end 
+  initial begin 
+    // Dump waves 
+    $dumpfile("dump.vcd");
+    $dumpvars(0); 
+  end endmodule
