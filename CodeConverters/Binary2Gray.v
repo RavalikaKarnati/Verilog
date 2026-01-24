cf511afd9a68623f82cc6d3c778323bb16fdf156
RavalikaKarnati <ravalika.karnati@gmail.com>
2026-01-24 11:55:15 -0500

    Comment out binary to gray conversion assignments

--- CodeConverters/Binary2Gray.v
+++ CodeConverters/Binary2Gray.v
@@ -1,3 +1,8 @@
+//assign  gray_out [3] = binary_in[3]; 
+//assign  gray_out [2] = binary_in[3] ^ binary_in[2]; 
+//assign gray_out [1] = binary_in[2] ^ binary_in[1]; 
+//assign  gray_out [0] = binary_in[1] ^ binary_in[0];
+
 module b2g_converter #(parameter WIDTH=4) (input [WIDTH-1:0] binary, output [WIDTH-1:0] gray);
   genvar i;    
   generate
